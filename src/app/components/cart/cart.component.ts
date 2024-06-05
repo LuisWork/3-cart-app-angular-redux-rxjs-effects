92137d2ddc89a538a02db62ddeec231c019a0866
Luis Zambrano <LuisZambrano.Work@gmail.com>
2024-06-05 03:11:23 -0400

    First commit

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartItem } from '../../models/cartItem';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent {
+
+  @Input() items: CartItem[] = [];
+
+  @Input() total = 0;
+
+  @Output() idProductEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCart(id: number) {
+    this.idProductEventEmitter.emit(id);
+  }
+
+}
